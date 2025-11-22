5484d640a522f039841a62c86dae7d0413cfbb20
kelma7800 <kelmacc913@gmail.com>
2025-11-22 02:36:08 -0800

    Add files via upload

---
+++ pyyyy1.py
@@ -0,0 +1,25 @@
+Guest=['Bennita', 'Tracey', 'Shekina']
+print("inviting my gurlies")
+print([0],' you are invited to dinner.')
+print('Tracey', "you are invited to dinner.")
+print('Shekina', "you are invited to dinner.")
+
+#unable to attend ; ='Tracey'
+print("Tracey is sorry cant make it")
+Guest[1]='Kelma'
+print(Guest)
+print(Guest[1], "you are invited to dinner.")
+print('Bennita', "you are invited to dinner.")
+print('Kelma', "you are invited to dinner.")
+print('Shekina', "you are invited to dinner.")
+#found a bigger table,so we need to add more guest
+print("good news gurlies, a bigger table was found")
+Guest.insert(0,"Solange")
+Guest.insert(2,"Dera")
+Guest.append("Emmanuella")
+print('Bennita', "you are invited to dinner.")
+print('Kelma', "you are invited to dinner.")
+print('Shekina', "you are invited to dinner.")
+print('Solange', "you are invited to dinner.")
+print('Dera', "you are invited to dinner.")
+print('Emmanuella', "you are invited to dinner.")
