aa4750fcd7b110f41bd133211f332e2e4b77d2e3
kelma7800 <kelmacc913@gmail.com>
2025-11-22 02:39:08 -0800

    Add files via upload

---
+++ Seeing the world.py
@@ -0,0 +1,31 @@
+places=['Britain', 'Dubai', 'America', 'Malawi', 'China']
+print("original list")
+print(places)
+print("")
+print(sorted(places))
+print("showing list is still in original order")
+print(places)
+print("using sorted to show the list is reversed")
+print(sorted(places,reverse=True))
+print(places)
+print("reversing the list")
+places.reverse()
+print(places)
+print("reversing the list again")
+places.reverse()
+print(places)
+print("using sort to change the oder of my list")
+places.sort()
+print(places)
+print("using sort reverse to change the other of the list")
+places.sort(reverse=True)
+print(places)
+
+
+
+
+
+
+
+
+
